180ce3d8d1c28cf54f5be1286ae8536bcbfba74c
Katie Moosman <moosman.katie@gmail.com>
2019-04-24 00:15:58 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    return redirect_to action: 'new' if params[:name].nil? or params[:name].empty?
+    # return redirect_to action: 'new' if params[:name].nil? or params[:name].empty?
     session[:name] = params[:name]
     redirect_to '/'
   end
